fb57898cb18bdd91bdac30827f1b1155b889a078
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-12-29 23:34:56 +0530

    :feat: typescritpt added to backend

---
+++ backend/dist/frontend/src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+function App() {
+    return (<div className="text-2xl">
+      <p>Hello World</p>
+    </div>);
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map

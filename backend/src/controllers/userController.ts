cc051e0761f2f004188a85e6f066ccf12dbf0634
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-01-22 03:01:02 +0530

    ':hammer: all user related routes created'

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,6 +2,7 @@ import { User } from "../models/userModel";
 import asyncHandler from "../middlewares/asyncHandler";
 import bcrypt from "bcrypt";
 import generateToken from "../utils/createToken";
+import { get } from "lodash";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -67,4 +68,96 @@ const getAllUsers = asyncHandler(async (req, res) => {
   res.status(200).json(users);
 });
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers };
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const currentUserId = get(req, "user._id") as string;
+  const user = await User.findById(currentUserId);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const currentUserId = get(req, "user._id") as string;
+  const user = await User.findById(currentUserId);
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+const deleteUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Cannot delete admin user.");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: "User deleted successfully." });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+const updateUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    await user.save();
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+};
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -4,6 +4,11 @@ import {
   getAllUsers,
   loginUser,
   logoutCurrentUser,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
 } from "../controllers/userController";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware";
 
@@ -15,5 +20,15 @@ router
   .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+// Admin routes
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;

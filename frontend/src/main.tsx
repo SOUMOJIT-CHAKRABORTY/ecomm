c7cf9e221b376ba8d79ac48fe44fe4834f123ca4
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-02-01 23:56:59 +0530

    :hammer: login,logout implemented using redux context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,12 +8,21 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import Login from "./pages/Auth/Login.tsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
 );
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );

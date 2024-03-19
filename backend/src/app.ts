021d1fe9b4d7b7ab19971cfcf0747470dcc88ece
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-03-19 23:46:30 +0530

    :fix: updated route handlers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,10 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import express from "express";
 import http from "http";
-import router from "./routes/userRoute";
+import userRouters from "./routes/userRoute";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
+import catagoryRouters from "./routes/categoryRoute";
 const app = express();
 const port = 3000;
 
@@ -20,7 +21,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/users", router);
+app.use("/api/users", userRouters);
+app.use("/api/category", catagoryRouters);
 const server = http.createServer(app);
 
 server.listen(port, () => {

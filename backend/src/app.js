05276fe41465bdff999abd30ad2b716ff2d41751
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-04-03 00:18:43 +0530

    :hammer: products page image uploded handled

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,9 @@ var app = (0, express_1.default)();
 var port = 3000;
 app.use(express_1.default.json());
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 app.use("/api/users", userRoute_1.default);
 app.listen(port, function () {
-    return console.log("Express is listening at http://localhost:".concat(port));
+  return console.log("Express is listening at http://localhost:".concat(port));
 });
--- frontend/src/pages/Admin/ProductList.tsx
+++ frontend/src/pages/Admin/ProductList.tsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import {
+  useCreateProductMutation,
+  useUploadProductImageMutation,
+} from "../../redux/api/productApiSlice";
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
+
 // type Props = {}
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [image, setImage] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [quantity, setQuantity] = useState<string>("");
+  const [brand, setBrand] = useState<string>("");
+  const [stock, setStock] = useState<number>(0);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const [uploadProductImage] = useUploadProductImageMutation();
+  const [createProduct] = useCreateProductMutation();
+  const { data: categories } = useFetchCategoriesQuery("Products");
+
+  return (
+    <div className="container xl:mx-[9rem] sm:mx-[0]">
+      <div className="flex flex-col md:flex-row">
+        {/* <AdminMenu /> */}
+        <div className="md:3/4 p-3">
+          <div className="h-12">Create Product</div>
+          {imageUrl && (
+            <div className="text-center">
+              <img
+                src={imageUrl}
+                alt="product"
+                className="block mx-auto max-h-[200px]"
+              />
+            </div>
+          )}
+
+          <div className="mb-3">
+            <label className="border text-white px-4 block w-full text-center rounded-lg cursor-pointer font-bold py-11">
+              {image ? image.name : "Upload Image"}
+            </label>
+            <input
+              type="file"
+              name="image"
+              accept="image/*"
+              // onChange={uploadFileHandler}
+              className={!image ? "hidden" : "text-white"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProductList;
